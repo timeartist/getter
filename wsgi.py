f2992e6e21a3654c074504a9b6345e8d546b0aa9
Adi Foulger <me@adi.uno>
2019-03-29 08:40:56 -0600

    change to getter

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,18 @@
-from flask import Flask
-from redis import Redis
-application = Flask(__name__)
-R = Redis(host="demo-db.redis1.svc.cluster.local", port=12345)
-#R = Redis()
+from json import loads, dumps
 
-@application.route("/")
-def hello():
-    return str(R.ping())
+from flask import Flask, render_template
+from redis import Redis
+
+application = Flask(__name__)
+redis = Redis(host="demo-db.redis1.svc.cluster.local", port=12345)
+
+@application.route('/')
+def show():
+    return render_template('display.html')
+
+@application.route('/data')
+def data():
+    return dumps(redis.lrange('form-inputs', 0, -1))
 
 if __name__ == "__main__":
     application.run()
